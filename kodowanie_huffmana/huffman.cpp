4709181ea41db93ed507678342b45fe60a6d23c4
Kuba <kuba.porebski3@gmail.com>
2024-02-02 23:45:10 +0100

    [PPK-18] Revert commit to master (#3)

--- kodowanie_huffmana/huffman.cpp
+++ kodowanie_huffmana/huffman.cpp
@@ -2,15 +2,12 @@
 #include "parameters.h"
 #include "appSettings.h"
 #include <map>
-#include <queue>
-
-using namespace std;
 
 void huffman::encode(char d[], int frequency[])
 {
 	int size = sizeof(d) / sizeof(d[0]);
 
-	priority_queue<MinHeapNode*, vector<MinHeapNode*> >priorityQ;
+	priority_queue<MinHeapNode*, vector<MinHeapNode*>, comparator> priorityQ;
 
 	for (int i = 0; i < size; i++)
 	{
@@ -25,15 +22,15 @@ void huffman::encode(char d[], int frequency[])
 		auto minFreqNode = priorityQ.top();
 		priorityQ.pop();
 		
-		auto secondMinFreqNode = priorityQ.top();
+		auto secondMinFreqNode = priorityQ.poll();
 		priorityQ.pop();
 
-		auto combinedFrequency = minFreqNode->frequency + secondMinFreqNode->frequency;
+		auto combinedFrequency = minFreqNode.frequency + secondMinFreqNode.frequency;
 		auto freq_node = new MinHeapNode('-', combinedFrequency);
-		freq_node->left = minFreqNode;
-		freq_node->right = secondMinFreqNode;
+		freq_node.left = minFreqNode;
+		freq_node.right = secondMinFreqNode;
 
-		root = *freq_node;
+		root = freq_node;
 
 		priorityQ.push(freq_node);
 	}
@@ -49,16 +46,36 @@ void huffman::sort() {
 
 	std::ifstream input(settings->inputFile);
 
-	if (input.is_open()) {
+	//if (input) {
+	//	std::string text;
+	//	int freq = 0;
+	//	char symbol;
+
+
+	//	while (input >> text) {
+	//		for (int i = 0; i < text.size(); i++) {
+	//			symbol = text[i];
+	//			freq++;
+	//			}
+	//		}
+
+	//	std::cout << symbol << freq << std::endl;
+	//	}
+	//}
+
+	if (input) {
 		char data;
 		std::map<char, int> freq;
-		while (input.get(data)) {
+		while (input) {
+			data = input.get();
 			freq[data]++;
 		}
 		input.close();
 
 		for (const auto& wpis : freq) {
 			std::cout << wpis.first << "   " << wpis.second << std::endl;
+			//this->minHN.character = wpis.first;
+			this->minHN.frequency = wpis.second;
 		}
 	}
 }
