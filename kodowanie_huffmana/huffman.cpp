7ba2d9efcd3f206deea1cf6ea4fb6f5342097361
Paciej <porebskimacio04@gmail.com>
2024-02-02 23:01:57 +0100

    feat(freq): frequency counter; fix: syntax fixes

--- kodowanie_huffmana/huffman.cpp
+++ kodowanie_huffmana/huffman.cpp
@@ -2,10 +2,42 @@
 #include "parameters.h"
 #include "appSettings.h"
 #include <map>
+#include <queue>
+
+using namespace std;
 
 void huffman::encode(char d[], int frequency[])
 {
 	int size = sizeof(d) / sizeof(d[0]);
+
+	priority_queue<MinHeapNode*, vector<MinHeapNode*> >priorityQ;
+
+	for (int i = 0; i < size; i++)
+	{
+		auto node = new MinHeapNode(d[i],frequency[i]);
+		priorityQ.push(node);
+	}
+
+	MinHeapNode root = NULL;
+
+	while (priorityQ.size() > 1)
+	{
+		auto minFreqNode = priorityQ.top();
+		priorityQ.pop();
+		
+		auto secondMinFreqNode = priorityQ.top();
+		priorityQ.pop();
+
+		auto combinedFrequency = minFreqNode->frequency + secondMinFreqNode->frequency;
+		auto freq_node = new MinHeapNode('-', combinedFrequency);
+		freq_node->left = minFreqNode;
+		freq_node->right = secondMinFreqNode;
+
+		root = *freq_node;
+
+		priorityQ.push(freq_node);
+	}
+	
 }
 
 bool huffman::compareToRight(MinHeapNode* l, MinHeapNode* r)
@@ -17,36 +49,16 @@ void huffman::sort() {
 
 	std::ifstream input(settings->inputFile);
 
-	//if (input) {
-	//	std::string text;
-	//	int freq = 0;
-	//	char symbol;
-
-
-	//	while (input >> text) {
-	//		for (int i = 0; i < text.size(); i++) {
-	//			symbol = text[i];
-	//			freq++;
-	//			}
-	//		}
-
-	//	std::cout << symbol << freq << std::endl;
-	//	}
-	//}
-
-	if (input) {
+	if (input.is_open()) {
 		char data;
 		std::map<char, int> freq;
-		while (input) {
-			data = input.get();
+		while (input.get(data)) {
 			freq[data]++;
 		}
 		input.close();
 
 		for (const auto& wpis : freq) {
 			std::cout << wpis.first << "   " << wpis.second << std::endl;
-			//this->minHN.character = wpis.first;
-			this->minHN.frequency = wpis.second;
 		}
 	}
 }
