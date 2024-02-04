67c2d24fa2a18489d3cd01fa36f0221305d3d63e
Kuba <kuba.porebski3@gmail.com>
2024-02-04 13:34:22 +0100

    doc: draft doc (#9)

--- kodowanie_huffmana/huffman.cpp
+++ kodowanie_huffmana/huffman.cpp
@@ -6,17 +6,21 @@
 
 using namespace std;
 
+/*
+ * główna funkcja dla kodowania huffmana, na końcu zapisuje plik o wskazanej nazwie z zakodowanym tekstem oraz drzewem
+ */
 EncodingResult* huffman::encode(char d[], int frequency[]) {
-	int size = sizeof(d) / sizeof(d[0]);
+	int size = sizeof(d) / sizeof(d[0]); // ilość elementów
 
-	priority_queue<MinHeapNode*, vector<MinHeapNode*>, Comparator> priorityQ;
+	priority_queue<MinHeapNode*, vector<MinHeapNode*>, Comparator> priorityQ; // kolejka z priorytetem na namniejszą częstotliwość wystąpień
 
+	//wrzucenie wszystkiego do kolejki
 	for (int i = 0; i < size; i++) {
 		auto node = new MinHeapNode(d[i],frequency[i]);
 		priorityQ.push(node);
 	}
 
-	MinHeapNode* root = nullptr;
+	MinHeapNode* root = nullptr; // korzeń drzewa
 
 	while (priorityQ.size() > 1) {
 		auto minFreqNode = priorityQ.top();
@@ -24,40 +28,48 @@ EncodingResult* huffman::encode(char d[], int frequency[]) {
 		
 		auto secondMinFreqNode = priorityQ.top();
 		priorityQ.pop();
+		// pobieramy dwa pierwsze node'y i usuwamy z kolejki
 
-		auto combinedFrequency = minFreqNode->frequency + secondMinFreqNode->frequency;
-		auto freq_node = new MinHeapNode('-', combinedFrequency);
-		freq_node->left = minFreqNode;
+		auto combinedFrequency = minFreqNode->frequency + secondMinFreqNode->frequency; // dodajemy do siebie ich częstotliwości
+		auto freq_node = new MinHeapNode('-', combinedFrequency); // tworzymy roboczy node, ponieważ interesują nas częstotliwości, placeholderem będzie '-'
+		freq_node->left = minFreqNode; // przypisujemy jako lewą i prawą gałąź pobrane node'y
 		freq_node->right = secondMinFreqNode;
 
-		root = freq_node;
+		root = freq_node; // nadpisujemy root by zbudować drzewo potrzebne do późniejszych kodowań
 
-		priorityQ.push(freq_node);
+		priorityQ.push(freq_node); // nowy node wrzucamy do kolejki
 	}
 	/* najwięcej ile może mieć drzewo wyokości w najmniej
 	 * optymalnym wariancie (winorośl czyli wszystkie node'y po jednej stronie)
 	 * ponieważ root nie jest liczony w wysokości drzewa
 	 */
 	int maxTreeHeight = size - 1;
-	int codes[maxTreeHeight];
-	generateCharacterCodes(root, codes, 0);
-	return new EncodingResult(encodeMessage('caban'), root);
+	int codes[maxTreeHeight]; // możliwa długość kodu dla znaku
+	generateCharacterCodes(root, codes, 0); // generowanie kodów huffmana dla znaków
+	return new EncodingResult(encodeMessage('caban'), root); // zwracamy wyniki generując skompresowaną wiadomość w locie
 }
 
+// kodowanie wiadomości z tablicy kodów
 char* huffman::encodeMessage(char* originalMessage) {
-	int index = 0;
 	char* result = nullptr;
 	
-	for (auto character : originalMessage) {
+	// dla każdego znaku znajdź jego wartość i dodaj do wiadomości.
+	// Znak służy jako klucz:
+	//		f: 01001,
+	//		a: 01
+	for (auto character : originalMessage) { 
 		result += huffmanCodes[character];
-		index++;
 	}
 	
 	return result;
 }
 
+//pobranie node'a z wiersza i przejście dalej do gałęzi potomnych, algorytm to deep first search (DFS)
+//powiedz kokotowi to się zesra
 MinHeapNode* huffman::readFromFileRec(MinHeapNode* root, const std::ifstream& file) {
 	char* value;
+	
+	// exit condition
 	if (!(file >> value)) {
 		return nullptr;
 	}
@@ -67,6 +79,7 @@ MinHeapNode* huffman::readFromFileRec(MinHeapNode* root, const std::ifstream& fi
 	node->right = readFromFileRec(root->right, file);
 }
 
+// to co wyżej tylko zapisywanie wierszy
 void huffman::writeToFileRec(MinHeapNode* root, const std::ofstream& file) {
 	if (root == nullptr) {
 		return;
@@ -78,28 +91,36 @@ void huffman::writeToFileRec(MinHeapNode* root, const std::ofstream& file) {
 	writeToFileRec(root->right, file);
 }
 
+/*
+ * stworzenie tablicy kodów mówiącej tyle, że
+ * od root musisz posuwać się w lewo jeżeli 0 i prawo jeżeli 1 czyli
+ * jeżeli mamy 110100 to będzie RRLRLL dla litery.
+ * Często pojawiające się znaki będą miały najkrótsze drogi typu f: 0
+ */
 void huffman::generateCharacterCodes(MinHeapNode *root, int* codesArray, int top) {
 	if (root->left) {
 		codesArray[top] = 0;
-		generateCharacterCodes(root->left, codesArray, top + 1);
+		generateCharacterCodes(root->left, codesArray, top + 1); //rekurencja, przechodzimy do następnej pozycji kodu dla literki i powtarzamy porównania 
 	} else if (root->right) {
 		codesArray[top] = 1;
 		generateCharacterCodes(root->right, codesArray, top + 1);
 	} else {
 		char codesCharacters[top];
 		for (int i = 0; i < top; i++) {
-			codesCharacters[i] = '0' + codesArray[i];;
+			codesCharacters[i] = '0' + codesArray[i]; //zmiana int na char poprzez dodanie znakowego 0. Śmieszny trick ogólnie w cpp.
 		}
-		huffmanCodes[root->character] = codesCharacters;
+		huffmanCodes[root->character] = codesCharacters; //przypisanie kodu do znaku
 	}
 }
 
+// główna funkcja do dekodowania
 DecodingResult* huffman::decode(MinHeapNode* root, char* encodedText) {
 	int textLength = sizeof(encodedText) / sizeof(encodedText[0]);
 	char* result = nullptr;
 
 	MinHeapNode* current = root;
 
+	// odwrócenie tablicy kodów z generateCharacterCodes()
 	for (int i = 0; i < textLength; i++) {
 		if (encodedText[i] == '0') {
 			current = current->left;
